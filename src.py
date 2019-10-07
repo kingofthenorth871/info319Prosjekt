546f956d5d6be99b96ffbed3aefdbe89230d93ac
LAP-TOR\Tor Aasheim <tor.aasheim@gmail.com>
2019-10-07 13:17:30 +0200

    Frequency test (memory error)

--- src.py
+++ src.py
@@ -5,15 +5,29 @@ from collections import Counter
 
 with open("tweets.json", encoding='utf-8') as file:
     data = json.load(file)
+#saves all the tweets in a list
+tweet = []
+for x in data:
+    tweet.append(x['tweets'])
+
+#creates a string to hold all the tweets for word count
+tweet_string = ''.join(tweet)
+
 
 #Counts word frequency for each tweet
 word_freq = []
-for x in data:
-    tokens = x['tweets'].split(" ")
+for x in tweet_string:
+    tokens = tweet_string.split(" ")
     cnt = Counter(tokens)
     freq = cnt.most_common()
     word_freq.append(freq)
 
+print(word_freq)
 
-print(word_freq[0][0][1])
+
+
+
+
+
+
 
