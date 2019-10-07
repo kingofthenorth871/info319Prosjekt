3fe74381c9323fca1f2aaef03e413bb107228884
kingofthenorth871 <35599641+kingofthenorth871@users.noreply.github.com>
2019-10-07 15:03:30 +0200

    Merge branch 'master' of https://github.com/kingofthenorth871/info319Prosjekt
    # Conflicts:
    #	.idea/info319Prosjekt.iml
    #	.idea/misc.xml
    #	untitled0.py

--- stemming.py
+++ stemming.py
@@ -1,5 +1,8 @@
 ## fra hit
 
+import codecs
+from collections import Counter
+
 import json
 import pandas as pd
 
@@ -8,28 +11,51 @@ nltk.download('punkt')
 
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
 
 ps = PorterStemmer()
-
-example_words = ["python", "pythoner", "pythoning", "pythoned", "Pythonly"]
-
-## for w in example_words:
-## print(ps.stem(w))
-
-new_text = "It is very important to be pythonly while you are pythoning with python. All Pythoners have Pythoned"
-
-words = word_tokenize(new_text)
-
-for w in words:
-    print(ps.stem(w))
 
 
 with open("tweets.json", encoding='utf-8') as file:
     data = json.load(file)
-
+#saves all the tweets in a list
+tweet = []
 for x in data:
-    freq = pd.Series((x['tweets']).split()).value_counts()[:20]
-    freq = pd.Series((x['tweets']).split()).value_counts()[:20]
-    freq
+    tweet.append(x['tweets'])
 
-## til hit
+##fra hit stemming og stopwords
+
+#creates a string to hold all the tweets for word count
+tweet_string = ''.join(tweet)
+
+words = word_tokenize(tweet_string)
+
+stemTweet = []
+for w in words:
+    stemTweet.append(ps.stem(w))
+
+tweet_string2 = ' '.join(stemTweet)
+
+stop_words = set(stopwords.words("english"))
+
+words2 = word_tokenize(tweet_string2)
+filtered_sentence = [w for w in words2 if not w in stop_words]
+
+tweet_string3 = ' '.join(filtered_sentence)
+
+##til hit stemming og stopwords
+
+#Counts word frequency for each tweet
+word_freq = []
+tokens = tweet_string2.split(" ")
+cnt = Counter(tokens)
+freq = cnt.most_common()
+word_freq.append(freq)
+
+
+print(word_freq)
+file_out = codecs.open("word_frq.txt", "w", "utf-8")
+file_out.write(str(word_freq))
+file_out.close()
