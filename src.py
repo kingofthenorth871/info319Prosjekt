140b1b3377e37efa8ded10edae2f42933074d485
kingofthenorth871 <35599641+kingofthenorth871@users.noreply.github.com>
2019-10-07 13:03:35 +0200

    Merge branch 'master' of https://github.com/kingofthenorth871/info319Prosjekt
    # Conflicts:
    #	.idea/info319Prosjekt.iml
    #	.idea/misc.xml
    #	untitled0.py

--- src.py
+++ src.py
@@ -1,3 +1,11 @@
+
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+ps = PorterStemmer()
+
+example_words = ["python", "pythoner", "pythoning", "pythoned", "Pythonly"]
+
 import json
 import pandas as pd
 
