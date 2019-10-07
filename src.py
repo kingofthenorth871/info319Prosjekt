641856387567ad723a9f8f51ea24af0b46bf97b2
LAP-TOR\Tor Aasheim <tor.aasheim@gmail.com>
2019-10-07 12:27:22 +0200

    Frequency for each tweet

--- src.py
+++ src.py
@@ -1,16 +1,19 @@
 import json
 import pandas as pd
+from collections import Counter
+
 
 with open("tweets.json", encoding='utf-8') as file:
     data = json.load(file)
 
+#Counts word frequency for each tweet
+word_freq = []
 for x in data:
-    freq = pd.Series((x['tweets']).split()).value_counts()[:20]
-    freq
+    tokens = x['tweets'].split(" ")
+    cnt = Counter(tokens)
+    freq = cnt.most_common()
+    word_freq.append(freq)
 
 
-
-
-print(freq)
-
+print(word_freq[0][0][1])
 
